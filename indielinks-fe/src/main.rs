f4fa40178cf7b37503384cb4fdf63f6cbe95bb98
Michael Herstine <sp1ff@pobox.com>
2026-04-24 10:42:31 -0700

    Revamp the App component. Add TailwindCSS.
    This commit revamps the top-level App component, which was written
    when I was new to Leptos (and front end development, altogether).
    
    It also introduces use of TailwindCSS for styling, though I haven't
    tied it into the packaging, yet.

--- indielinks-fe/src/main.rs
+++ indielinks-fe/src/main.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Michael Herstine <sp1ff@pobox.com>
+// Copyright (C) 2025-2026 Michael Herstine <sp1ff@pobox.com>
 //
 // This file is part of indielinks.
 //
@@ -75,16 +75,17 @@
 use gloo_net::http::Request;
 use leptos::prelude::*;
 use leptos_router::{
-    components::{ProtectedRoute, Route, Router, Routes},
+    components::{ProtectedRoute, Route, Router, Routes, A},
     hooks::{use_location, use_navigate},
-    location::State,
-    path, NavigateOptions,
+    //     location::State,
+    path,
+    NavigateOptions,
 };
 use thaw::{Layout, LayoutHeader, Link, Tab, TabList};
 use tracing::{debug, error, info, Level};
 use tracing_subscriber::fmt;
 use tracing_subscriber_wasm::MakeConsoleWriter;
-use wasm_bindgen::JsValue;
+// use wasm_bindgen::JsValue;
 
 use indielinks_shared::api::REFRESH_CSRF_COOKIE;
 
@@ -98,13 +99,13 @@ use indielinks_fe::{
     types::{Api, Base, PageSize, Token, USER_AGENT},
 };
 
-/// [indielinks-fe](crate) root component
+/// [App] is [indielinks-fe](crate) root component; `main()` will mount it as the DOM body.
 #[component]
 fn App() -> impl IntoView {
     // Setup the network location at which we can reach indielinks. In my world (the backend), this
     // would be a configuration item, typically provided by something as sophisticated as a
     // parameter management system, or as simple as a configuration file. In this world
-    // (frontend/CSR/SPA) there are a few options, including fetching, at runtime a configuration
+    // (frontend/CSR/SPA) there are a few options, including fetching, at runtime, a configuration
     // file from the same endpoint at which this WASM bundle is being served.
     //
     // For now, I'm going to go with the simplest option, which is to read environment variables;
@@ -125,148 +126,102 @@ fn App() -> impl IntoView {
     provide_context(Base(
         option_env!("INDIELINKS_BASE").unwrap_or("").to_owned(),
     ));
+    let base = expect_context::<Base>();
 
-    // OK-- we store the access token here. Perhaps make this into a context, as well? At this
-    // level, we need R/W access, but in subordinate `View`s, perhaps the accessor could be provided
-    // via context.
+    // OK-- we store the access token here, but I'm probably going to revisit, since it needs to be
+    // refreshed periodically. Something else to consider at that time: should this be a
+    // `SecretString`, instead? Also, why am I making this an `RwSignal`?
+    provide_context(Token::new(None));
+    let token = expect_context::<Token>();
 
-    provide_context(RwSignal::<Option<String>>::new(None));
+    let selected_value = RwSignal::new("instance".to_owned());
 
-    // Now, on a fresh load, we *may* have a refresh token laying around-- let's try it:
-
-    // Not sure if this is required?
-    let selected_value = RwSignal::new("home".to_owned());
-
-    let token = use_context::<Token>().expect("No token Cell!?");
-
-    let base = use_context::<Base>().expect("No base for the API!?").0;
-    let s = format!("{base}/s");
-    let u = format!("{base}/u");
-    // I have *no idea* why I need to use these, here.
-    let i = StoredValue::new(format!("{base}/"));
-    let h = StoredValue::new(format!("{base}/h"));
-    let f = StoredValue::new(format!("{base}/f"));
-
-    // I'm interested in the `ErrorBoundary` component as a means of error handling.
-
-    // The constant cloning is getting *really* tiresome:
-    let t0 = token.clone();
-    let t1 = token.clone();
-    // let t2 = token.clone();
-    // let t3 = token.clone();
-    // let t4 = token.clone();
-
-    // Seems prolix...
-    async fn refresh() -> bool {
-        match refresh_token().await {
-            Ok(_) => {
-                info!("Successfully obtained an access token!");
-                true
-            }
-            Err(err) => {
-                error!("While refreshing token: {err:?}");
-                false
-            }
-        }
-    }
     // If I don't use a `LocalResource`; if I, say, just call `refresh_token()` directly in the
     // `Await` below, I get pages of warnings about the future not being Send (?)
-    let fetcher = LocalResource::new(|| refresh());
+    let try_token_refresh = LocalResource::new(|| async move { refresh_token().await.is_ok() });
 
-    let t5 = token.clone();
-    let api = use_context::<Api>().expect("No API!?").0;
+    // let t5 = token.clone();
     let on_sign_out = Action::new_unsync(move |_: &()| {
-        let api = api.clone();
-        let token = token.get().expect("No token!?");
-        t5.set(None);
         wasm_cookies::delete(REFRESH_CSRF_COOKIE);
         async move {
+            let api = expect_context::<Api>().0;
+            let tokens = token.get().expect("No token!?");
             let rsp = Request::post(&format!("{api}/api/v1/users/logout"))
                 .credentials(web_sys::RequestCredentials::Include)
                 .header("User-Agent", USER_AGENT)
-                .header("Authorization", &format!("Bearer {}", token))
+                .header("Authorization", &format!("Bearer {}", tokens))
                 .send()
                 .await
                 .map_err(|err| err.to_string())
                 .and_then(string_for_status);
             info!("Logged-out: {rsp:?}");
+            token.set(None);
         }
     });
 
     view! {
-        <Await
-            future=fetcher.into_future()
-            let:_data>
-            <Router base>
-                {
-                    let location = use_location(); // -> Location (which is Clone, but that's it)
-                    let navigate = use_navigate(); // -> impl Fn(&str, NavigateOptions) + Clone
-                    let add_link =
-                        move |_| {
-                            let state = web_sys::js_sys::Object::new();
-                            let _ = web_sys::js_sys::Reflect::set(
-                                &state,
-                                &"from".into(),
-                                &JsValue::from(location.pathname.get()),
-                            );
-                            navigate(
-                                "/a",
-                                NavigateOptions {
-                                    state: State::new(Some(state.into())),
-                                    ..Default::default()
-                                },
-                            );
-                    };
+        // I thought to use the `Await` component while waiting for the token refresh to complete,
+        // but the future it demands must be send, and ultimately, the future returned by
+        // `refresh_token()` is not. Not sure what I want to use for a fallback, however.
+        <Suspense fallback=move || view! { <p>"Attempting a token refresh"</p> }>
 
-                    view! {
-                        // I'm using a `thaw` component here, `Layout` (https://thawui.vercel.app/components/layout)
-                        <LayoutHeader class="w-full flex bg-sky-600">
-                            <h1 class="text-sky-100 pt-[4px] pl-[8px]">indielinks</h1>
-                            <Show when=move || t0.get().is_some() >
-                                <TabList selected_value class="pl-16px">
-                                    // Should I really be using `Link` here?
-                                    <Tab value="instance" class="text-sky-100 m-6px"><Link href={ i.read_value().clone() }>"Instance"</Link></Tab>
-                                    <Tab value="home" class="text-sky-100 m-6px"><Link href={ h.read_value().clone() }>"Home"</Link></Tab>
-                                    <Tab value="feeds" class="text-sky-100 m-6px"><Link href={ f.read_value().clone() }>"Feeds"</Link></Tab>
-                                </TabList>
-                            </Show>
-                            <Show when = move || t1.get().is_none() && use_location().pathname.get() != "/s" >
-                                <div class="auth-actions">
+            <Router base=base.0>
+                <LayoutHeader class="w-full font-header text-sky-100 flex bg-sky-600">
+
+                    <span class="text-sky-100 text-6xl pt-[4px] pb-[4px] pl-[8px] pr-[18px]">indielinks</span>
+
+                    // Tab list running across the top when logged in:
+                    <Show when=move || token.get().is_some() >
+                        <TabList selected_value>
+                            <Tab value="instance" class="m-[6px]">
+                                <A href="/">"instance"</A>
+                            </Tab>
+                            <Tab value="home" class="m-[6px]">
+                                <A href="/h">"home"</A>
+                            </Tab>
+                            <Tab value="feeds" class="m-[6px]">
+                                <A href="/f">"feeds"</A>
+                            </Tab>
+                        </TabList>
+                    </Show>
+
+                    // Links on the top RHS; what's shown depends on whether or not the user
+                    // is logged-in
+                    <Show when=move || use_location().pathname.get() != "/s">
+                        <Show when=move || token.get().is_some()
+                              // Worth it to factor this out into its own component?
+                              fallback=|| view!{
+                                <div class="auth-actions mt-[4px] mr-[8px] ml-auto">
                                     <ul style="list-style-type: none; font-size: smaller;">
-                                    <li><a href={ s.clone() }>"sign-in"</a></li>
-                                    <li><a href={ u.clone() }>"sign-up"</a></li>
+                                    <li><A href="/s">"sign-in"</A></li>
+                                    <li><A href="/u">"sign-up"</A></li>
                                     </ul>
                                 </div>
-                            </Show>
-                            <Show when = move || token.get().is_some() && use_location().pathname.get() != "/s" >
-                                <div class="auth-actions pr-8px ml-auto text-sky-100">
-                                    <ul style="list-style-type: none; font-size: smaller;">
-                                        // Note the `clone()` in the on:click handler-- this is
-                                        // essential! TBH, I'm a bit hazy on why, but it has to do
-                                        // with the fact that we're inside a `<Show>` component,
-                                        // here.
-                                        <li><a href="#" on:click=add_link.clone()>"add link"</a></li>
-                                        <li><a href="#" on:click=move |_| {
-                                            on_sign_out.dispatch(());
-                                        }>"sign-out"</a></li>
-                                    </ul>
-                                </div>
-                            </Show>
-                        </LayoutHeader>
-                    }
-                }
+                              }>
+                            // Same here-- make this its own component?
+                            <div class="auth-actions mt-[4px] mr-[8px] ml-auto">
+                                <ul style="list-style-type: none; font-size: smaller;">
+                                    // Should these be <buttons>?
+                                    <li><A href="/a">"add link"</A></li>
+                                    <li><a href="#" on:click=move |_| { on_sign_out.dispatch(()); }>
+                                        "sign-out"</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </Show>
+                    </Show>
+                </LayoutHeader>
                 <Layout>
-                    <main>
                         <Routes fallback=Instance>
                             <Route path=path!("/") view=Instance />
-                            <Route path=path!("/s") view=move || view!{ <SignIn /> } />
+                            <Route path=path!("/s") view=SignIn />
                             <ProtectedRoute
                                 path=path!("/h")
                                 // Some(true) means display, Some(false) means do *not* display, and
                                 // None means that this information is still loading
                                 condition = move || Some(token.get().is_some())
                                 redirect_path = || "/"
-                                view=move || view! { <Home />}
+                                view=Home
                             />
                             <ProtectedRoute
                                 path=path!("/f")
@@ -285,10 +240,9 @@ fn App() -> impl IntoView {
                                 view=AddLink
                             />
                         </Routes>
-                    </main>
                 </Layout>
             </Router>
-        </Await>
+        </Suspense>
     }
 }
 
@@ -305,7 +259,7 @@ fn main() {
     // the browser with an unhelpful message like "Unreachable executed" and a stack trace that
     // points into your WASM binary. With `console_error_panic_hook`, we'll get an actual Rust stack
     // trace that includes a line in the Rust source code
-    // (https://book.leptos.dev/getting_started/leptos_dx.html).
+    // <https://book.leptos.dev/getting_started/leptos_dx.html>.
     console_error_panic_hook::set_once();
     // Mount the `App` component as the document <body>:
     leptos::mount::mount_to_body(App);
